045194cc3d2c336ef1087532a8d3b3a3ca50993f
Roger-luo <rogerluo.rl18@gmail.com>
2020-09-10 13:56:15 -0400

    fix build issue

--- src/PlutoUtils.jl
+++ src/PlutoUtils.jl
@@ -23,11 +23,11 @@ open a Pluto notebook at given path.
 @cast function open(file; host="127.0.0.1", port::Int=1234, launchbrowser::Bool=false, project=nothing)
     isfile(file) || error("file $file does not exist!")
     s = Pluto.ServerSession()
+    config = Pluto.ServerConfiguration(launch_browser=launchbrowser)
+
     nb = Pluto.SessionActions.open(s, file; project=project)
-
-    @info "you can open the notebook at: http://localhost:$port/edit?id=$(nb.notebook_id)"
-    Pluto.run(host, port;launchbrowser=launchbrowser, session=s)
-
+    println("you can open the notebook at: http://localhost:$port/edit?id=$(nb.notebook_id)")
+    Pluto.run(host, port;configuration=config, session=s)
     return
 end
 
@@ -45,11 +45,12 @@ start Pluto notebook server.
 - `-l,--launchbrowser`: add this flag to launch browser.
 """
 @cast function run(;host="127.0.0.1", port::Int=1234, launchbrowser::Bool=false, project=nothing)
+    config = Pluto.ServerConfiguration(launch_browser=launchbrowser)
     s = ServerSession(default_environment_path=project)
-    Pluto.run(host, port; launchbrowser=launchbrowser, session=s)
+    Pluto.run(host, port; configuration=config, session=s)
 end
 
 
-@main doc="Pluto CLI - Lightweight reactive notebooks for Julia"
+@main name="pluto" doc="Pluto CLI - Lightweight reactive notebooks for Julia"
 
 end
