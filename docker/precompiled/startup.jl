43913fa31632a6fd7366e706489ff6053ee5c613
Benjamin Lungwitz <benj.lung@googlemail.com>
2020-10-07 19:56:22 +0200

    removed versioninfo - not working in container

--- docker/precompiled/startup.jl
+++ docker/precompiled/startup.jl
@@ -1,4 +1,3 @@
-versioninfo()
 using Pluto
 server_options = Pluto.Configuration.ServerOptions(; host="0.0.0.0", port=1234, launch_browser=false)
 security_options = Pluto.Configuration.SecurityOptions(; require_secret_for_access=false)
