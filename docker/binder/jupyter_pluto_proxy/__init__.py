555d5d4742d752718f08c0c8c2f6138de35bf3ac
Fons van der Plas <fonsvdplas@gmail.com>
2020-10-15 13:14:51 +0200

    __init__.py to use Pluto 0.12 syntax

--- docker/binder/jupyter_pluto_proxy/__init__.py
+++ docker/binder/jupyter_pluto_proxy/__init__.py
@@ -2,7 +2,7 @@ import os
 
 def setup_pluto():
     def _get_pluto_cmd(port):
-        return ["julia", "-e", "import Pluto; Pluto.run(\"127.0.0.1\", " + str(port) + ")"]
+        return ["julia", "-e", "import Pluto; Pluto.run(host=\"127.0.0.1\", port=" + str(port) + ", launch_browser=false, require_secret_for_access=false)"]
 
     return {
         "command": _get_pluto_cmd,
