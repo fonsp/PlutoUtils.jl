a632e63171e60c7cf4f5fd85e8fe4c10d9001f40
Fons van der Plas <fonsvdplas@gmail.com>
2021-02-10 14:26:59 +0100

    Refactor, added kwargs
    Co-Authored-By: Leo <cacate0129@gmail.com>

--- src/Export.jl
+++ src/Export.jl
@@ -1,5 +1,7 @@
 module Export
 
+export github_action, export_paths, generate_html
+
 import Pluto
 import Pluto: ServerSession
 using HTTP
@@ -8,58 +10,34 @@ using SHA
 using Sockets
 
 
-using Logging: global_logger
-using GitHubActions: GitHubActionsLogger
-get(ENV, "GITHUB_ACTIONS", "false") == "true" && global_logger(GitHubActionsLogger())
-
 myhash = base64encode ∘ sha256
 
-function github_action(; export_dir=".", offer_binder=false, copy_to_temp_before_running=false, disable_ui=true, bind_server_url=nothing, binder_url=nothing)
-    mkpath(export_dir)
+"""
+Search recursively for all Pluto notebooks in the current folder, and for each notebook:
+- Run the notebook and wait for all cells to finish
+- Export the state object
+- Create a .html file with the same name as the notebook, which has:
+  - The JS and CSS assets to load the Pluto editor
+  - The state object embedded
+  - Extra functionality enabled, such as hidden UI, binder button, and a live bind server
 
-    jlfiles = vcat(map(walkdir(".")) do (root, dirs, files)
-        map(
-            filter(files) do file
-                occursin(".jl", file)
-            end
-            ) do file
-            joinpath(root, file)
-        end
-    end...)
-    notebookfiles = filter(jlfiles) do f
-        readline(f) == "### A Pluto.jl notebook ###"
+# Arguments
+- `export_dir::String="."`: folder to write generated HTML files to (will create directories to preserve the input folder structure). Leave at the default `"."` to generate each HTML file in the same folder as the notebook file.
+- `disable_ui::Bool=true`: hide all buttons and toolbars to make it look like an article.
+- `baked_state::Bool=true`: base64-encode the state object and write it inside the .html file. If `false`, a separate `.plutostate` file is generated.
+- `offer_binder::Bool=false`: show a "Run on Binder" button on the notebooks. Use `binder_url` to choose a binder repository.
+- `binder_url::Union{Nothing,String}=nothing`: e.g. `https://mybinder.org/v2/gh/mitmath/18S191/e2dec90` TODO docs
+- `bind_server_url::Union{Nothing,String}=nothing`: e.g. `https://bindserver.mycoolproject.org/` TODO docs
+
+Additional keyword arguments will be passed on to the configuration of `Pluto`. See [`Pluto.Configuration`](@ref) for more info.
+"""
+function export_paths(notebook_paths::Vector{String}; export_dir=".", baked_state=true, copy_to_temp_before_running=false, offer_binder=false, disable_ui=true, bind_server_url=nothing, binder_url=nothing, kwargs...)
+    # TODO how can we fix the binder version to a Pluto version? We can't use the Pluto hash because the binder repo is different from Pluto.jl itself. We can use Pluto versions, tag those on the binder repo.
+    if offer_binder && binder_url === nothing
+        @warn "We highly recommend setting the `binder_url` keyword argument with a fixed commit hash. The default is not fixed to a specific version, and the binder button will break when Pluto updates.
+        
+        This might be automated in the future."
     end
-    export_paths(notebookfiles; export_dir=export_dir, copy_to_temp_before_running=copy_to_temp_before_running, offer_binder=offer_binder, disable_ui=disable_ui, bind_server_url=bind_server_url, binder_url=binder_url)
-
-    create_default_index(;export_dir=export_dir)
-end
-
-function create_default_index(;export_dir=".")
-    default_md = """
-    Notebooks:
-
-    <ul>
-        {% for page in site.static_files %}
-            {% if page.extname == ".html" %}
-                <li><a href="{{ page.path | absolute_url }}">{{ page.name }}</a></li>
-            {% endif %}
-        {% endfor %}
-    </ul>
-
-    <br>
-    <br>
-    <br>
-    """
-
-    exists = any(["index.html", "index.md", "index.jl"]) do f
-        joinpath(export_dir, f) |> isfile
-    end
-    if !exists
-        write(joinpath(export_dir, "index.md"), default_md)
-    end
-end
-
-function export_paths(notebook_paths::Vector{String}; export_dir=".", copy_to_temp_before_running=false, offer_binder=false, disable_ui=true, bind_server_url=nothing, binder_url=nothing, kwargs...)
     export_dir = Pluto.tamepath(export_dir)
 
     options = Pluto.Configuration.from_flat_kwargs(; kwargs...)
@@ -75,26 +53,27 @@ function export_paths(notebook_paths::Vector{String}; export_dir=".", copy_to_te
             else
                 newpath = path
             end
-            nb = Pluto.SessionActions.open(session, newpath; run_async=false)
+            # open and run the notebook
+            notebook = Pluto.SessionActions.open(session, newpath; run_async=false)
 
             @info "Ready $(path)" hash
 
-            html_filename = if endswith(path, ".jl")
-                path[1:end-3] * ".html"
-            else
-                path * ".html"
-            end
+            html_path = without_pluto_file_extension(path) * ".html"
 
-            export_path = joinpath(export_dir, html_filename)
+            export_path = joinpath(export_dir, html_path)
             export_jl_path = joinpath(export_dir, path)
             mkpath(dirname(export_path))
 
 
             notebookfile_js = if offer_binder
+                if !isfile(export_jl_path)
+                    write(export_jl_path, read(path))
+                end
                 repr(basename(path))
             else
                 "undefined"
             end
+
             bind_server_url_js = if bind_server_url !== nothing
                 repr(bind_server_url)
             else
@@ -105,16 +84,35 @@ function export_paths(notebook_paths::Vector{String}; export_dir=".", copy_to_te
             else
                 "undefined"
             end
-            html_contents = generate_baked_html(nb; notebookfile_js=notebookfile_js, disable_ui=disable_ui, bind_server_url_js=bind_server_url_js, binder_url_js=binder_url_js)
 
-            write(export_path, html_contents)
-            if offer_binder && !isfile(export_jl_path)
-                write(export_jl_path, read(path))
+            state = Pluto.notebook_to_js(notebook)
+
+            statefile_js = if !baked_state
+                statefile_path = without_pluto_file_extension(path) * ".plutostate"
+                open(statefile_path, "w") do io
+                    Pluto.pack(io, state)
+                end
+                repr(basename(statefile_path))
+            else
+                statefile64 = base64encode() do io
+                    Pluto.pack(io, state)
+                end
+
+                "\"data:;base64,$(statefile64)\""
             end
 
+
+            html_contents = generate_html(; 
+                notebookfile_js=notebookfile_js, statefile_js=statefile_js,
+                bind_server_url_js=bind_server_url_js, binder_url_js=binder_url_js,
+                disable_ui=disable_ui
+            )
+
+            write(export_path, html_contents)
+            
             @info "Written to $(export_path)"
 
-            Pluto.SessionActions.shutdown(session, nb)
+            Pluto.SessionActions.shutdown(session, notebook)
         catch e
             @error "$path failed to run" exception=(e, catch_backtrace())
         end
@@ -123,37 +121,12 @@ function export_paths(notebook_paths::Vector{String}; export_dir=".", copy_to_te
 end
 
 
-import Pkg
-function try_get_pluto_version()
-    try
-        deps = Pkg.API.dependencies()
-
-        p_index = findfirst(p -> p.name == "Pluto", deps)
-        p = deps[p_index]
-
-        if p.git_revision === nothing
-            p.version
-        else
-            # ugh
-            is_probably_a_commit_thing = all(in(('0':'9') ∪ ('a':'f')), p.git_revision)
-            if !is_probably_a_commit_thing
-                error("Do not add the Pluto dependency by specifying its BRANCH, but by specifying its VERSION or an exact COMMIT SHA.")
-            end
-
-            p.git_revision
-        end
-    catch e
-        @error "Failed to get exact Pluto version from dependency. Your website is not guaranteed to work forever." exception=(e, catch_backtrace())
-        Pluto.PLUTO_VERSION
-    end
-end
-
-
-function generate_baked_html(notebook::Pluto.Notebook; version=nothing, notebookfile_js="undefined", bind_server_url_js="undefined", binder_url_js="undefined", disable_ui=true)
-    state = Pluto.notebook_to_js(notebook)
-    statefile64 = base64encode() do io
-        Pluto.pack(io, state)
-    end
+function generate_html(;
+        version=nothing, 
+        notebookfile_js="undefined", statefile_js="undefined", 
+        bind_server_url_js="undefined", binder_url_js="undefined", 
+        disable_ui=true
+    )::String
 
     original = read(Pluto.project_relative_path("frontend", "editor.html"), String)
 
@@ -176,7 +149,7 @@ function generate_baked_html(notebook::Pluto.Notebook; version=nothing, notebook
         <script data-pluto-file="launch-parameters">
         window.pluto_notebookfile = $(notebookfile_js)
         window.pluto_disable_ui = $(disable_ui ? "true" : "false")
-        window.pluto_statefile = "data:;base64,$(statefile64)"
+        window.pluto_statefile = $(statefile_js)
         window.pluto_bind_server_url = $(bind_server_url_js)
         window.pluto_binder_url = $(binder_url_js)
         </script>
@@ -186,6 +159,117 @@ function generate_baked_html(notebook::Pluto.Notebook; version=nothing, notebook
 
     return result
 end
+
+
+
+
+## GITHUB ACTION
+
+using Logging: global_logger
+using GitHubActions: GitHubActionsLogger
+get(ENV, "GITHUB_ACTIONS", "false") == "true" && global_logger(GitHubActionsLogger())
+
+"A convenience function to call from a GitHub Action. See [`export_paths`](@ref) for the list of keyword arguments."
+function github_action(; export_dir=".", generate_default_index=true, kwargs...)
+    export_dir = Pluto.tamepath(export_dir)
+
+    mkpath(export_dir)
+
+    jlfiles = vcat(
+        map(walkdir(".")) do (root, dirs, files)
+            map(filter(endswith_pluto_file_extension, files)) do file
+                joinpath(root, file)
+            end
+        end...
+    )
+    notebookfiles = filter(jlfiles) do f
+        readline(f) == "### A Pluto.jl notebook ###"
+    end
+    export_paths(notebookfiles; export_dir=export_dir, kwargs...)
+
+    generate_default_index && create_default_index(;export_dir=export_dir)
+end
+
+"If no index.hmtl, index.md, index.jl file exists, create a default index.md that GitHub Pages will render into an index page, listing all notebooks."
+function create_default_index(;export_dir=".")
+    default_md = """
+    Notebooks:
+
+    <ul>
+        {% for page in site.static_files %}
+            {% if page.extname == ".html" %}
+                <li><a href="{{ page.path | absolute_url }}">{{ page.name }}</a></li>
+            {% endif %}
+        {% endfor %}
+    </ul>
+
+    <br>
+    <br>
+    <br>
+    """
+
+    exists = any(["index.html", "index.md", ("index"*e for e in pluto_file_extensions)...]) do f
+        joinpath(export_dir, f) |> isfile
+    end
+    if !exists
+        write(joinpath(export_dir, "index.md"), default_md)
+    end
+end
+
+
+
+
+
+## HELPERS
+
+const pluto_file_extensions = [
+    ".pluto.jl",
+    ".jl",
+    ".plutojl",
+    ".pluto",
+]
+
+endswith_pluto_file_extension(s) = any(endswith(s, e) for e in pluto_file_extensions)
+
+function without_pluto_file_extension(s)
+    for e in pluto_file_extensions
+        if endswith(s, e)
+            return s[1:end-length(e)]
+        end
+    end
+    s
+end
+
+
+import Pkg
+function try_get_pluto_version()
+    try
+        deps = Pkg.API.dependencies()
+
+        p_index = findfirst(p -> p.name == "Pluto", deps)
+        p = deps[p_index]
+
+        if p.is_tracking_registry
+            p.version
+        elseif p.is_tracking_path
+            error("Do not add the Pluto dependency as a local path, but by specifying its VERSION or an exact COMMIT SHA.")
+        else
+            # ugh
+            is_probably_a_commit_thing = all(in(('0':'9') ∪ ('a':'f')), p.git_revision)
+            if !is_probably_a_commit_thing
+                error("Do not add the Pluto dependency by specifying its BRANCH, but by specifying its VERSION or an exact COMMIT SHA.")
+            end
+
+            p.git_revision
+        end
+    catch e
+        @error "Failed to get exact Pluto version from dependency. Your website is not guaranteed to work forever." exception=(e, catch_backtrace())
+        Pluto.PLUTO_VERSION
+    end
+end
+
+
+
 
 
 
